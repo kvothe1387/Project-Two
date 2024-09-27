e24c2f14b5617d3dd9028039c8a41a711b424d26
David McCullough <dmccullough488@gmail.com>
2024-09-26 20:22:45 -0500

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <main className='container '>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/dist/models/user.js
@@ -0,0 +1,44 @@
+import { DataTypes, Model } from 'sequelize';
+import bcrypt from 'bcrypt';
+export class User extends Model {
+    // Hash the password before saving the user
+    async setPassword(password) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(password, saltRounds);
+    }
+}
+export function UserFactory(sequelize) {
+    User.init({
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    }, {
+        tableName: 'users',
+        sequelize,
+        hooks: {
+            beforeCreate: async (user) => {
+                await user.setPassword(user.password);
+            },
+            beforeUpdate: async (user) => {
+                if (user.changed('password')) {
+                    await user.setPassword(user.password);
+                }
+            },
+        },
+    });
+    return User;
+}
